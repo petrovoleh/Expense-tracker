58992d911b14e4e832b30b29c31a6c372b754131
Petrov Oleh <81036385+petrovoleh@users.noreply.github.com>
2024-05-06 18:23:53 +0300

    save analytics to csv and pdf methods

--- src/client/app/src/main/java/com/expensetracker/data/Analytics.java
+++ src/client/app/src/main/java/com/expensetracker/data/Analytics.java
@@ -1,34 +1,42 @@
 package com.expensetracker.data;
 
+import androidx.documentfile.provider.DocumentFile;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.layout.Document;
+import com.itextpdf.layout.element.Paragraph;
 
 import com.expensetracker.MainActivity;
 import com.expensetracker.database.AppDatabase;
 import com.expensetracker.database.TransactionDao;
 import com.expensetracker.models.Category;
 
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-import kotlinx.coroutines.CoroutineScope;
 
 public class Analytics {
     private String text;
     private final TransactionDao transactionDao;
     private final String[] names = Categories.getCategoriesNames();
+
     public Analytics() {
         text = new String();
         AppDatabase database = MainActivity.getDatabase();
         transactionDao = database.transactionDao();
     }
 
-    public String getAllAnalytics(){
+    public String getAllAnalytics() {
         StringBuilder builder = new StringBuilder();
-        for (String name:names){
+        for (String name : names) {
 
             getAnalytics(name, result -> builder.append(result).append("\n"));
 
@@ -37,23 +45,23 @@ public class Analytics {
         return text;
     }
 
-    public int getAllocated(String categoryName){
+    public int getAllocated(String categoryName) {
         Category category = Categories.getCategory(categoryName);
         assert category != null;
         return category.getBudget();
     }
 
-        public int getAvailable(String categoryName) {
+    public int getAvailable(String categoryName) {
         Category category = Categories.getCategory(categoryName);
         assert category != null;
         int all = category.getBudget();
 
         // Observe the LiveData for total value
         Double spent = transactionDao.getTotalValueForCategory(categoryName);
-            if (spent == null) {
-                spent = 0.0;
-            }
-            return (int) (all - spent);
+        if (spent == null) {
+            spent = 0.0;
+        }
+        return (int) (all - spent);
     }
 
     public void getAnalytics(String categoryName, AnalyticsCallback callback) {
@@ -71,10 +79,10 @@ public class Analytics {
             }
 
             // Calculate available budget
-            int available = (int)(all - spent);
+            int available = (int) (all - spent);
 
             // Prepare result
-            String result = categoryName + " " + all + " " +  spent.intValue() + " " + available;
+            String result = categoryName + " " + all + " " + spent.intValue() + " " + available;
 
             // Deliver the result on the main thread
             callback.onAnalyticsResult(result);
@@ -83,40 +91,90 @@ public class Analytics {
             executor.shutdown();
         });
     }
+
     public void setText(String text) {
         this.text = text;
     }
+
     public String getText() {
         return text;
     }
+
     public interface AnalyticsCallback {
         void onAnalyticsResult(String result);
     }
-    public String[] getNames(){
+
+    public String[] getNames() {
         return names;
     }
 
-    public int size(){
+    public int size() {
         return names.length;
     }
+
     // Method to save all analytics to a CSV file
-    public void saveAllAnalyticsToCSV(File csvFile) throws IOException {
-        FileWriter writer = new FileWriter(csvFile);
-        // Write header
-        writer.append("Category,Budget,Spent,Available\n");
-        for (String name : names) {
-            Category category = Categories.getCategory(name);
-            assert category != null;
-            int budget = category.getBudget();
-            Double spent = transactionDao.getTotalValueForCategory(name);
-            if (spent == null) {
-                spent = 0.0;
+    public void saveAllAnalyticsToCSV(OutputStream outputStream) throws IOException {
+
+        if (outputStream != null) {
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(outputStream));
+
+            // Write header
+            writer.write("Category,Budget,Spent,Available\n");
+
+            for (String name : names) {
+                Category category = Categories.getCategory(name);
+                assert category != null;
+                int budget = category.getBudget();
+                Double spent = transactionDao.getTotalValueForCategory(name);
+                if (spent == null) {
+                    spent = 0.0;
+                }
+                int available = (int) (budget - spent);
+                // Write data
+                writer.write(name + "," + budget + "," + spent.intValue() + "," + available + "\n");
             }
-            int available = (int) (budget - spent);
-            writer.append(name).append(",").append(String.valueOf(budget)).append(",")
-                    .append(String.valueOf(spent.intValue())).append(",").append(String.valueOf(available)).append("\n");
+
+            writer.flush();
+            writer.close();
+            outputStream.close();
+        } else {
+            throw new IOException("Failed to open output stream");
         }
-        writer.flush();
-        writer.close();
+    }
+
+    public void saveAllAnalyticsToPDF(OutputStream outputStream) throws IOException {
+        PdfDocument pdfDoc = new PdfDocument(new PdfWriter(outputStream));
+        Document document = new Document(pdfDoc);
+
+        try {
+            // Add title
+            document.add(new Paragraph("Expense Tracker Analytics"));
+
+            // Add header
+            document.add(new Paragraph("Category   Budget   Spent   Available"));
+
+            for (String name : names) {
+                Category category = Categories.getCategory(name);
+                assert category != null;
+                int budget = category.getBudget();
+                Double spent = transactionDao.getTotalValueForCategory(name);
+                if (spent == null) {
+                    spent = 0.0;
+                }
+                int available = (int) (budget - spent);
+                // Add data
+                document.add(new Paragraph(name + "   " + budget + "   " + spent.intValue() + "   " + available));
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (document != null) {
+                document.close();
+            }
+            if (outputStream != null) {
+                outputStream.close();
+            }
+        }
     }
 }
--- src/client/app/src/main/java/com/expensetracker/ui/analytics/AnalyticsFragment.java
+++ src/client/app/src/main/java/com/expensetracker/ui/analytics/AnalyticsFragment.java
@@ -1,11 +1,21 @@
 package com.expensetracker.ui.analytics;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
+import androidx.documentfile.provider.DocumentFile;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -15,21 +25,37 @@ import com.expensetracker.data.Analytics;
 import com.expensetracker.adapters.AnalyticsAdapter;
 import com.expensetracker.databinding.FragmentAnalyticsBinding;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class AnalyticsFragment extends Fragment {
 
     private FragmentAnalyticsBinding binding;
     private RecyclerView recyclerViewAnalytics;
     private AnalyticsAdapter analyticsAdapter;
     private Analytics analytics;
+    private Button saveCSVButton;
+    private Button savePDFButton;
+
+    private String type = "csv";
+    // Activity result launcher for SAF folder selection
+    private ActivityResultLauncher<Intent> folderSelectionLauncher;
+
+    private ActivityResultLauncher<Intent> folderSelectionLauncher2;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-
 
         binding = FragmentAnalyticsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
         recyclerViewAnalytics = root.findViewById(R.id.recyclerViewAnalytics);
         recyclerViewAnalytics.setLayoutManager(new LinearLayoutManager(getContext()));
+        saveCSVButton = root.findViewById(R.id.saveCSV);
+        savePDFButton = root.findViewById(R.id.savePDF);
 
         // Create an adapter with an empty list of transactions
         this.analytics = new Analytics();
@@ -37,13 +63,59 @@ public class AnalyticsFragment extends Fragment {
         analyticsAdapter = new AnalyticsAdapter(analytics);
         recyclerViewAnalytics.setAdapter(analyticsAdapter);
 
+        // Initialize activity result launcher for SAF folder selection
+        folderSelectionLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        Intent data = result.getData();
+                        if (data != null && data.getData() != null) {
+                            Uri uri = data.getData();
+                            saveAnalytics(uri);
+                        }
+                    }
+                });
+        saveCSVButton.setOnClickListener(v -> selectFolder("csv")); // Call method to select folder on button click
+        savePDFButton.setOnClickListener(v -> selectFolder("pdf")); // Call method to select folder on button click
+
         return root;
     }
 
-    void getAnalytics(){
+    void getAnalytics() {
         analytics.getAllAnalytics();
     }
 
+    private void selectFolder(String type) {
+        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT_TREE);
+        this.type = type;
+        folderSelectionLauncher.launch(intent);
+    }
+
+    private void saveAnalytics(Uri folderUri) {
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        executor.execute(() -> {
+            try {
+                DocumentFile folder = DocumentFile.fromTreeUri(getContext(), folderUri);
+                DocumentFile file = folder.createFile("text/" + (type.equals("csv") ? "csv" : "pdf"), "analytics." + type);
+                OutputStream outputStream = getContext().getContentResolver().openOutputStream(file.getUri());
+
+                if (type.equals("csv")) {
+                    analytics.saveAllAnalyticsToCSV(outputStream);
+                } else if (type.equals("pdf")) {
+                    analytics.saveAllAnalyticsToPDF(outputStream);
+                }
+
+                getActivity().runOnUiThread(() -> writeToast("Analytics saved to " + type.toUpperCase() + " successfully"));
+            } catch (IOException e) {
+                e.printStackTrace();
+                getActivity().runOnUiThread(() -> writeToast("Failed to save analytics to " + type.toUpperCase()));
+            }
+            executor.shutdown();
+        });
+    }
+
+    private void writeToast(String text) {
+        Toast.makeText(getContext(), text, Toast.LENGTH_SHORT).show();
+    }
 
     @Override
     public void onDestroyView() {
